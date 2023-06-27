801031f960968f504182837b4fcb5b78b4cc2cae
Hary <haingotue@gmail.com>
2023-06-27 11:12:27 +0700

    update code to the layolut of result page

---
+++ src/components/body/SearchResult/NavBar/FiltersNav/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './FiltersNav.css';
+
+const FiltersNav = () => {
+  return (
+    <div
+      className='filers-nav-container'
+    >FiltersNav</div>
+  )
+}
+
+export default FiltersNav

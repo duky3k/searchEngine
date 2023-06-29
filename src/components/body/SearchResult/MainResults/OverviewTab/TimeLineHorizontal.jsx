346b88685f7060c996c10f707d0ebe74496f0255
LDAP\nmduc6 <nmduc6@cmcglobal.com>
2023-06-29 15:46:10 +0700

    config redux

--- src/components/body/SearchResult/MainResults/OverviewTab/TimeLineHorizontal.jsx
+++ src/components/body/SearchResult/MainResults/OverviewTab/TimeLineHorizontal.jsx
@@ -1,12 +1,17 @@
-import { Steps } from 'antd';
-import { useState } from 'react';
-import { fakeDataTimeLineHorizontal } from '../../../../../constant/constants';
+import { Steps } from "antd";
+import { useState } from "react";
+import { fakeDataTimeLineHorizontal } from "../../../../../constant/constants";
+import { useDispatch } from "react-redux";
+import { incrementByAmount } from "../../../../../store/slices/counterSlice";
 
 const TimeLineHorizontal = () => {
   const [current, setCurrent] = useState(0);
+  const dispatch = useDispatch();
+
   const onChange = (value) => {
-    console.log('onChange:', value);
+    console.log("onChange:", value);
     setCurrent(value);
+    dispatch(incrementByAmount(current));
   };
   return (
     <>
